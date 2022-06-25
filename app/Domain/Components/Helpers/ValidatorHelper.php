5b689dd15f0b40ca579355dfb46ad420f43f02e3
lucas castro <lucas.castro@hotmail.com>
2022-06-25 19:14:23 -0300

    create helpers

---
+++ app/Domain/Components/Helpers/ValidatorHelper.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Components\Helpers;
+
+class ValidatorHelper
+{
+    public static function validateCpf(string $attribute, string $value): bool
+    {
+        $c = preg_replace('/\D/', '', trim($value));
+
+        if (strlen($c) != 11 || preg_match("/^{$c[0]}{11}$/", $c)) {
+            return false;
+        }
+
+        for ($s = 10, $n = 0, $i = 0; $s >= 2; $n += $c[$i++] * $s--);
+
+        if ($c[9] != ((($n %= 11) < 2) ? 0 : 11 - $n)) {
+            return false;
+        }
+
+        for ($s = 11, $n = 0, $i = 0; $s >= 2; $n += $c[$i++] * $s--);
+
+        if ($c[10] != ((($n %= 11) < 2) ? 0 : 11 - $n)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static function validateCnpj(string $attribute, string $value): bool
+    {
+        $value = trim($value);
+        $c = preg_replace('/\D/', '', $value);
+
+        $b = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+
+        if (strlen($c) != 14) {
+            return false;
+        } elseif (preg_match("/^{$c[0]}{14}$/", $c) > 0) {
+            return false;
+        }
+
+        for ($i = 0, $n = 0; $i < 12; $n += $c[$i] * $b[++$i]);
+
+        if ($c[12] != ((($n %= 11) < 2) ? 0 : 11 - $n)) {
+            return false;
+        }
+
+        for ($i = 0, $n = 0; $i <= 12; $n += $c[$i] * $b[$i++]);
+
+        if ($c[13] != ((($n %= 11) < 2) ? 0 : 11 - $n)) {
+            return false;
+        }
+
+        return true;
+    }
+}
